33f7a3259dc23f7bf0dcf9018a558997ca915d74
franciscodara <franciscodara@gmail.com>
2020-05-17 02:16:50 -0300

    Frontend de formulário de cadastro de tarefa

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
 
 def listar_tarefa(request):
     nome_tarefa = "Olá Dara, essa é sua primeira tarefa!"
     return render(request, 'tarefas/listar_tarefas.html', {"nome_tarefa": nome_tarefa})
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
