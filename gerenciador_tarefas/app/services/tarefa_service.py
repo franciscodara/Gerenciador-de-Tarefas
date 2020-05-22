1ddb3e8e44695dccfcc3e00bb85ea7f6ff4d663a
franciscodara <franciscodara@gmail.com>
2020-05-22 01:31:20 -0300

    Autenticação padrão Django e relacionamento user/task.

--- gerenciador_tarefas/app/services/tarefa_service.py
+++ gerenciador_tarefas/app/services/tarefa_service.py
@@ -1,9 +1,9 @@
 from ..models import Tarefa
 
 def cadastrar_tarefa(tarefa):
-    Tarefa.objects.create(titulo=tarefa.titulo, descricao=tarefa.descricao, data_expiracao=tarefa.data_expiracao, prioridade=tarefa.prioridade)
-def listar_tarefas():
-    return Tarefa.objects.all()
+    Tarefa.objects.create(titulo=tarefa.titulo, descricao=tarefa.descricao, data_expiracao=tarefa.data_expiracao, prioridade=tarefa.prioridade, usuario=tarefa.usuario)
+def listar_tarefas(usuario):
+    return Tarefa.objects.filter(usuario=usuario) .all()
 def listar_tarefa_id(id):
     return Tarefa.objects.get(id=id)
 def editar_tarefa(tarefa_bd, tarefa_nova):
--- gerenciador_tarefas/app/views/tarefa_views.py
+++ gerenciador_tarefas/app/views/tarefa_views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import TarefaForm
-from .entidades.tarefa import Tarefa
-from .services import tarefa_service
+from ..forms import TarefaForm
+from ..entidades.tarefa import Tarefa
+from ..services import tarefa_service
 
 # Create your views here.
 
+@login_required()
 def listar_tarefa(request):
-    tarefas = tarefa_service.listar_tarefas()
+    tarefas = tarefa_service.listar_tarefas(request.user)
     return render(request, 'tarefas/listar_tarefas.html', {"tarefas": tarefas})
 
+@login_required()
 def cadastrar_tarefa(request):
     if request.method == "POST":
         form_tarefa = TarefaForm(request.POST)
@@ -17,24 +21,30 @@ def cadastrar_tarefa(request):
             descricao = form_tarefa.cleaned_data["descricao"]
             data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
             prioridade = form_tarefa.cleaned_data["prioridade"]
-            tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade)
+            tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade, usuario=request.user)
             tarefa_service.cadastrar_tarefa(tarefa_nova)
             return redirect('listar_tarefas')
     else:
         form_tarefa = TarefaForm()
     return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
+
+@login_required()
 def editar_tarefa(request, id):
     tarefa_db = tarefa_service.listar_tarefa_id(id)
+    if tarefa_db.usuario != request.user:
+        return HttpResponse('Não permitido.')
     form_tarefa = TarefaForm(request.POST or None, instance=tarefa_db)
     if form_tarefa.is_valid():
         titulo = form_tarefa.cleaned_data["titulo"]
         descricao = form_tarefa.cleaned_data["descricao"]
         data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
         prioridade = form_tarefa.cleaned_data["prioridade"]
-        tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade)
+        tarefa_nova = Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao, prioridade=prioridade, usuario=request.user)
         tarefa_service.editar_tarefa(tarefa_db, tarefa_nova)
         return redirect('listar_tarefas')
     return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
+
+@login_required()
 def remover_tarefa(request, id):
     tarefa_db = tarefa_service.listar_tarefa_id(id)
     if request.method == "POST":
